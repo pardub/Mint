f4eac8b8615c06797ab4ebb580d13f83b317dc6f
pardub <42610272+pardub@users.noreply.github.com>
2022-12-20 19:28:58 +0000

    Update install2_global.sh

--- install2_global.sh
+++ install2_global.sh
@@ -295,6 +295,13 @@ sudo apt install -y qtpass
 
 # Download Visual Studio Code
 
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo apt-get -y install apt-transport-https
+sudo apt-get -y update
+sudo apt-get install -y code 
+
 # +----------------+
 # | Virtualization |
 # +----------------+
