413477523342519bcaa08bd89bcdbac0c5842b3a
pardub <42610272+pardub@users.noreply.github.com>
2022-12-20 17:04:41 +0000

    Create install1.sh

---
+++ install1.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -xev
+
+# +---------+
+# | Install |
+# +---------+
+
+# Setup Visudo
+echo "$USER ALL=(ALL:ALL) ALL" | sudo EDITOR='tee -a' visudo
+echo "Defaults:$USER timestamp_timeout=120" | sudo EDITOR='tee -a' visudo
+
+# Add dns from https://dns.watch/
+sudo echo 'nameserver 84.200.69.80' | sudo tee -a /etc/resolv.conf
+sudo echo 'nameserver 84.200.70.40' | sudo tee -a /etc/resolv.conf
+
+mkdir -p "$HOME"/.config/zsh
+cd || exit
+
+# sudo reboot
