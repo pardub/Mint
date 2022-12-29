f92e3c357ba1e6052c7bf9e44e00e8b79dc95370
pardub <42610272+pardub@users.noreply.github.com>
2022-12-29 18:54:08 +0000

    Update install3_docker.sh

--- install3_docker.sh
+++ install3_docker.sh
@@ -7,22 +7,20 @@ sudo apt-get install \
     curl \
     gnupg \
     lsb-release
-    
-sudo mkdir -p /etc/apt/keyrings
-sudo apt -y install docker*
-  
-sudo apt-get update
 
-sudo systemctl start docker
-sudo systemctl enable docker
-
-# Add USER to docker group
-sudo usermod -aG docker ${USER}
+ sudo apt update -y
+ 
+# Install Docker CE
+sudo apt -y install apt-transport-https ca-certificates curl software-properties-common
+sudo apt -y remove docker docker-engine docker.io containerd runc
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu jammy stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update -y
+sudo apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo usermod -aG docker $USER
 
 # To activate Docker completion
 sudo mv /usr/share/zsh/vendor-completions/_docker /usr/share/zsh/site-functions
 
 # Delete .zcompdump
 rm -f "$HOME"/.config/zsh/.zcompdump
-
-
