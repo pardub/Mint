aa2589b22c645fd48c074ab07d7c756cc2266d57
pardub <42610272+pardub@users.noreply.github.com>
2023-04-13 11:58:43 +0000

    Update install2_global.sh

--- install2_global.sh
+++ install2_global.sh
@@ -347,7 +347,8 @@ cd || exit
 # INSTALL JETBRAINS MONO FONTS IN ~/.local/share/fonts
 mkdir -p "$HOME"/.local/share/fonts/nerd-fonts
 cd /tmp
-sudo wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/JetBrainsMono.zip
+sudo wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.2/JetBrainsMono.zip
+#sudo wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/JetBrainsMono.zip
 #sudo wget https://github.com/ryanoasis/nerd-fonts/releases/download/2.2.0-RC/JetBrainsMono.zip
 sudo unzip /tmp/JetBrainsMono.zip -d ~/.local/share/fonts/nerd-fonts
 fc-cache -v
